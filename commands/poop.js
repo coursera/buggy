af2bacce59fc2f6390c7d56f999f4df5c8d618b1
eleith <online+git@eleith.com>
2016-02-01 13:58:12 -0800

    make message public

--- commands/poop.js
+++ commands/poop.js
@@ -3,5 +3,7 @@ var Message = require('../slack/message');
 var Command = require('../slack/command');
 
 module.exports = new Command(/poop|poo/, function(slack, jira, context) {
-  response.send(slack.response_url, new Message(':poop:'), context.done);
+  var message = new Message(':poop:');
+  message.setResponseType(true);
+  response.send(slack.response_url, message, context.done);
 });
